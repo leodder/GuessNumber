f29132b1943bcb7ee46f32ffa388ed2623304cb1
leodder <a0919989772@gmail.com>
2025-09-15 19:11:50 +0900

    feat: button component

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,47 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 const PrimaryButton = ({ children }) => {
+  const pressHandler = () => {
+    console.log("Pressed");
+  };
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640223" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    // borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    // margin: 4,
+    elevation: 2, // android's shadow css
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -15,14 +21,25 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#72063c',
+    backgroundColor: "#4e0329",
     borderRadius: 8,
     elevation: 4, // android's shadow css
-    shadowColor: 'black', // ios's shadow color
-    shadowOffset: {width: 0, height: 2}, // how much the shadow should be offset from the original object to which it belongs to the left and the right
+    shadowColor: "black", // ios's shadow color
+    shadowOffset: { width: 0, height: 2 }, // how much the shadow should be offset from the original object to which it belongs to the left and the right
     shadowRadius: 6, // control how much the shadow expands
     shadowOpacity: 0.25, // control how transparent a shadow is
-  }
-})
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
 
 export default StartGameScreen;
