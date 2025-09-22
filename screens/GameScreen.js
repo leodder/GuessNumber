e6d1851984706fe27a9d237638a43ca124c06743
leodder <a0919989772@gmail.com>
2025-09-22 17:28:44 +0900

    feat: install react-native-safe-area-context

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,26 @@
 // import React from 'react'
-import { Text } from "react-native" 
+import { View, Text, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const GameScreen = () => {
   return (
-    <Text>GameScreen</Text>
-  )
-}
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* Guess */}
+      <View>
+        <Text>Higher or Lower</Text>
+      </View>
+      {/* + - */}
+      {/* <View>LOG Rounds</View> */}
+    </View>
+  );
+};
 
-export default GameScreen
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
