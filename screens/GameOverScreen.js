6be4fa388caf24251d9ad0db3c172de9c3163d2d
leodder <a0919989772@gmail.com>
2025-09-27 02:29:56 +0900

    feat: gmaeover screen add img

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,8 +1,17 @@
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Image } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 const GameOverScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text>Game is over!</Text>
+      <Title>Game is over!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y</Text>
     </View>
   );
 };
@@ -12,8 +21,23 @@ export default GameOverScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    marginTop: 50,
+    // marginTop: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
     // marginHorizontal: 24,
-    padding: 16,
+    padding: 24,
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
